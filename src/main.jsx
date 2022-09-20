cedb6d3a9cc036ac7abb755c690e30b2eae15ec3
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-20 13:14:14 -0300

    Fix: learn about useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,12 @@ import ReactDOM from 'react-dom/client';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffec/Layout';
-import { Memorize } from './06-memos/Memorize';
+// import { Memorize } from './06-memos/Memorize';
+import { MemorizeHook } from './06-memos/MemorizeHook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <Memorize />
+  <MemorizeHook />
   // </React.StrictMode>
 );
