83e109aaaa72aecb2a32eaafb56b234e288531c4
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-21 16:19:11 -0300

    Fix: order with components

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,21 +1,26 @@
 import { useReducer } from 'react';
+import { TodoAdd, TodoList } from './components';
 import { todoReducer } from './todoReducer';
 
 const initialState = [
   {
-    id: new Date().getTime,
+    id: new Date().getTime(),
     description: 'Recolectar la piedra del alma',
     done: false,
   },
   {
-    id: new Date().getTime * 2,
-    description: 'Recolectar la piedra del alma',
+    id: new Date().getTime() + 2,
+    description: 'Recolectar la piedra del tiempo',
     done: false,
   },
 ];
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+  const handleNewTodo = (todo) => {
+    console.log({ todo });
+  };
 
   return (
     <>
@@ -26,30 +31,14 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <ul className="list-group">
-            {todos.map((todo) => (
-              <li key={todo.id} className="list-group-item d-flex justify-content-between">
-                <span className="align-self-center">item1</span>
-                <button className="btn btn-danger">borrar</button>
-              </li>
-            ))}
-          </ul>
+          <TodoList todos={todos} />
         </div>
 
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
 
-          <form>
-            <input
-              type="text"
-              placeholder="Que hay que hacer?"
-              className="form-contol"
-            />
-            <button type="submit" className="btn btn-outline-primary mt-1">
-              Agregar
-            </button>
-          </form>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
---
+++ src/08-useReducer/components/TodoAdd.jsx
@@ -0,0 +1,40 @@
+import { useForm } from '../../hooks/useForm';
+
+export const TodoAdd = ({ onNewTodo }) => {
+  const { description, onInputChange, onResetForm } = useForm({
+    description: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validar que no este vacio
+    if (description.length <= 1) return;
+
+    const newTodo = {
+      id: new Date().getTime() + 2,
+      description,
+      done: false,
+    };
+
+    onNewTodo(newTodo)
+
+    onResetForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Que hay que hacer?"
+        className="form-contol"
+        name="description"
+        value={description}
+        onChange={onInputChange}
+      />
+      <button type="submit" className="btn btn-outline-primary mt-1">
+        Agregar
+      </button>
+    </form>
+  );
+};
