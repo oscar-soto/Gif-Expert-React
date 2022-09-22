b4c1458083903e68ba3d979e42c7f5737a88ca46
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-22 00:05:16 -0300

    Fix: Delete todo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -35,6 +35,14 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>
@@ -44,7 +52,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
 
         <div className="col-5">
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -3,7 +3,9 @@ export const todoReducer = (initalState = [], action) => {
     case '[TODO] Add Todo':
       // throw new Error('Action.type = ABC no esta implementada');
       return [...initalState, action.payload];
-      
+    case '[TODO] Remove Todo':
+      return initalState.filter((todo) => todo.id !== action.payload);
+
     default:
       return initalState;
   }
