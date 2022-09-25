65b5140bbe03a43ba20163ff239450c1c691e1fd
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-24 22:22:50 -0300

    Fix: learn about Link an NavLin

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,27 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { NavBar } from './NavBar';
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link> */}
+      <NavBar />
       <hr />
-      
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+
+        {/* <Route path='/*' element={ <LoginPage />} /> */}
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
