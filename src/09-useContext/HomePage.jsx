61643915adb7862c5ad455fc86474e0de07c4c74
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-26 01:08:16 -0300

    Fix: Testing to HomePage

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,17 +1,16 @@
-import { useContext } from "react"
-import { UserContext } from "./context/UserContex"
+import { useContext } from 'react';
+import { UserContext } from './context/UserContex';
 
 export const HomePage = () => {
-  const {user} = useContext(UserContext)
-  console.log(user)
+  const { user } = useContext(UserContext);
 
   return (
     <>
-      <h1>HomePage <small>{user?.name}</small> </h1>
+      <h1>
+        HomePage <small>{user?.name}</small>{' '}
+      </h1>
       <hr />
-      <pre>
-        {JSON.stringify( user, null ,3 )}
-      </pre>
+      <pre aria-label="pre">{JSON.stringify(user, null, 3)}</pre>
     </>
-  )
-}
+  );
+};
